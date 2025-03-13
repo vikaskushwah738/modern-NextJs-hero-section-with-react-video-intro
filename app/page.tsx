5d5ea87fe247a3cf62f29a444df1674afee37cbd
vikaskushwah738 <vikaskushwah738@gmail.com>
2025-03-13 17:30:44 +0530

    font size changes

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ const Home = () => {
         </video>
 
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
-          <h1 className="text-5xl font-medium pb-4">Expert Care, Every Step of the Way.</h1>
+          <h1 className="text-4xl font-medium pb-4">Expert Care, Every Step of the Way.</h1>
           <button
             className="px-5 py-2 text-lg font-semibold text-black bg-white rounded-full hover:bg-blue-500 hover:text-white transition duration-500">
             Book Appointment</button>
